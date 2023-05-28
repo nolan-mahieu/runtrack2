a0e40e4949807bcb71c71028ef3d84abc3991251
nolan.mahieu@laplateforme.io <nolan.mahieu@laplateforme.io>
2023-05-29 00:12:03 +0200

    premier

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,21 +1,21 @@
 <?php
     // nous créons les variables $str, $str2 et $str3, puis nous les concaténons pour affiche le texte.
     $str = "LaPlateforme";
-    echo $str;
-
+    echo $str . "<br>";
+    
     $str2 = "Vive";
     $str3 = "!";
-    echo $str2 . " " . $str . " " . $str3;
-
+    echo $str2 . " " . $str . " " . $str3 . "<br>";
+    
     // nous créons la variable $val avec la valeur 6, l'affichons, puis ajoutons 4 à cette valeur et l'affichons à nouveau.
     $val = 6;
-    echo $val;
+    echo $val . "<br>";
     $val += 4;
-    echo $val;
+    echo $val . "<br>";
 
     // nous créons la variable $myBool avec la valeur true, l'affichons, puis modifions sa valeur en false et l'affichons à nouveau.
     $myBool = true;
-    echo $myBool;
+    echo $myBool . "<br>";
     $myBool = false;
-    echo $myBool;
+    echo $myBool . "<br>";
 ?>
