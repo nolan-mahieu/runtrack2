617e26ca9afdc610fa92ba62eea3651d58497cfc
nolan.mahieu@laplateforme.io <nolan.mahieu@laplateforme.io>
2023-05-29 00:43:52 +0200

    premier

---
+++ jour02/job05/index.php
@@ -0,0 +1,21 @@
+<?php
+function estPremier($nombre) {
+  if ($nombre < 2) {
+    return false;
+  }
+
+  for ($i = 2; $i <= sqrt($nombre); $i++) {
+    if ($nombre % $i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+for ($i = 2; $i <= 1000; $i++) {
+  if (isset($i) && estPremier($i)) {
+    echo $i . "<br />";
+  }
+}
+?>
