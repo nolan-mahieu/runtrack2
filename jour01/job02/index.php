14354acf66fc7694173c39ebfc4c39540f2f5bfa
nolan.mahieu@laplateforme.io <nolan.mahieu@laplateforme.io>
2023-05-29 00:16:23 +0200

    premier

---
+++ jour01/job02/index.php
@@ -0,0 +1,8 @@
+<?php
+  // Dans ce fichier, faites afficher deux fois “Hello LaPlateforme!”
+  echo "Hello LaPlateforme!"; // Commentaire en ligne
+  /*
+    Commentaire en bloc
+  */
+  echo "Hello LaPlateforme!"; // Commentaire en ligne
+?>
