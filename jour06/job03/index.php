6241b948e516ca2d2785167e38d748ae68900dd0
nolan.mahieu@laplateforme.io <nolan.mahieu@laplateforme.io>
2023-05-31 11:54:23 +0200

    premier

---
+++ jour06/job03/index.php
@@ -0,0 +1,8 @@
+<?php
+function getHello() {
+    return "Hello LaPlateforme!";
+}
+
+$message = getHello(); // Appel de la fonction et récupération de la valeur de retour
+echo $message; // Affichage de la valeur retour
+?>
